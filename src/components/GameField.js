d888e9a4bf9af7a43dcca44e8bc3e945d4883b40
VartanovMaks <vartanov2009@gmail.com>
2021-08-02 12:21:58 +0300

    commit before forking

--- src/components/GameField.js
+++ src/components/GameField.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-
-import game from '../data'
 import Word from './word';
 
-function GameField() {
+function GameField({task}) {
     
     return (
         <Container>
             <Row className="justify-content-md-center">
                 {
-                    game[0].words.map((item, index) => <Word word={`Слово ${index+1}`} key={Math.random()}/>)
+                    task.map((item, index) => <Word word={`Слово ${index+1}`} key={Math.random()}/>)
                 }   
             </Row>
         </Container>
--- src/components/SelectTask.js
+++ src/components/SelectTask.js
@@ -8,11 +8,13 @@ import GameField from './GameField';
 
 function SelectTask() {
     const [task, setTask] = useState(game[0]);
-
+    
     function categoryChoosen({target}) {
-        console.log(target.selectedIndex);
+        console.log(target[target.selectedIndex]);
+        // target[target.selectedIndex].disabled = true;
+        setTask(game[target.selectedIndex]);
     }
-
+   
     return (
         <div>
             <Container>
@@ -21,17 +23,17 @@ function SelectTask() {
                 </Row>
                 <Row className="justify-content-sm-center">
                     <Col md="auto" sm="auto">
-                    <Form.Group controlId="category">
-                        <Form.Control as="select" onChange={categoryChoosen}  >
-                            {
-                                game.map((item,index) => <option key={index}>{item.category}</option>)
-                            }
-                        </Form.Control>
+                        <Form.Group controlId="category">
+                            <Form.Control as="select" onChange={categoryChoosen}  >
+                                {
+                                    game.map((item,index) => <option key={index} >{item.category}</option>)
+                                }
+                            </Form.Control>
                         </Form.Group>
                     </Col>
                 </Row>
-            </Container>
-            <GameField/>
+            </Container >
+            <GameField task={task.words}/>
         </div>
     );
 }
