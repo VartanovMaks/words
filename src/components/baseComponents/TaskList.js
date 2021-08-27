8137b68487cb3d0d12ab12b563e9e5097920b5a5
VartanovMaks <vartanov2009@gmail.com>
2021-08-27 17:43:50 +0300

    select/unselect task done. Ready to save game

--- src/components/baseComponents/TaskList.js
+++ src/components/baseComponents/TaskList.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {Container, Button,ListGroup } from 'react-bootstrap';
+import {Container, Button,ListGroup, Col } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 
 function TaskList() {
 
     const [tasks, setTasks]=useState([]);
     const [counter, setCounter]= useState(0);
+
+    const [selectedTaskIndexes, setSelectedTaskIndexes]=useState([]);
 
 const fetchAllTasks = async () => {
     try{
@@ -22,28 +24,50 @@ const fetchAllTasks = async () => {
     fetchAllTasks();
   },[]);
 
-  function onSelectTask (e){
-    let taskId = e.target.parentElement.id;
-    if (!taskId) taskId = e.target.id
-    
-    const element = tasks.find(item=>item._id === taskId);
-    console.log(element);
-    setCounter(prev=>prev+1);
+  const onSaveGame = () =>{
+    prompt (" записываем игру");
+  }
+
+  const onButtonClick = (e) => {
+      let taskIndex=Number(e.target.id);
+      let index = selectedTaskIndexes.findIndex(item => item === taskIndex);
+      if (index > -1){
+          let arr = selectedTaskIndexes;
+          arr.splice(index, 1);
+          setSelectedTaskIndexes(arr);
+          e.target.innerText = "Select";
+          const tmpStyle={...e.target.style, backgroundColor:"#FFFFFF" }
+          e.target.style=tmpStyle;
+          setCounter(prev=>prev-1);
+        } else {
+            let arr = selectedTaskIndexes;
+            if (selectedTaskIndexes.length > 11 ) {
+                return
+            }
+            arr.push(taskIndex);
+            setSelectedTaskIndexes(arr);
+            setCounter(prev=>prev+1);
+            e.target.innerText = "Selected"
+            e.target.style.backgroundColor="#12EE5B";
+        }
   }
 
     return (
         <Container>
         <Row className="justify-content-md-start">
-            <Button variant="info" className="my-1 mx-3">Записать игру</Button>
+            <Button variant="info" className="my-1 mx-3" onClick={onSaveGame}>Записать игру</Button>
             <h5>Выбрано {counter} заданий</h5>
         </Row>
         <Row className="justify-content-sm-center mt-2">
             <ListGroup className="tasks-list">
                 {tasks.map((item,index)=>(
-                    <ListGroup.Item action variant={index % 2 ? "info":"secondary"} key={item._id} id={item._id} onClick={onSelectTask}>
+                    <div className="task-item" key={index}>
+                        <Button variant={index % 2 ? "outline-info":"outline-secondary"} id={index} onClick={onButtonClick}>Select</Button>
+                    <ListGroup.Item action  variant={index % 2 ? "info":"secondary"}>
                         <h5 className="my-0">Задание: {item.task}</h5>
                         <p className="my-0"> Фраза : {item.words.join(' ')}</p>
                     </ListGroup.Item>
+                    </div>
                 ))}
             </ListGroup>
         </Row>
